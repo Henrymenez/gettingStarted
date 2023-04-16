2591d9a33dac0877fcdf59dd0002003eeaf8f3d8
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2023-04-16 21:18:43 +0100

    two way binding  binding

--- testApp/src/app/test/test.component.ts
+++ testApp/src/app/test/test.component.ts
@@ -5,18 +5,18 @@ import { Component } from '@angular/core';
   selector: '.app-test',
   template: `<div>Welcome {{ name }}</div>
     <hr />
-    <h2>{{ 2 + 2 }}</h2>
+    <!-- <h2>{{ 2 + 2 }}</h2> -->
     <!-- class binding -->
-    <h2 [class]="successClass">{{ 'Welcome ' + name }}</h2>
+    <!-- <h2 [class]="successClass">{{ 'Welcome ' + name }}</h2>
     <h2>{{ name.length }}</h2>
-    <h2>{{ name.toUpperCase() }}</h2>
+    <h2>{{ name.toUpperCase() }}</h2> -->
     <!-- binding more than one class -->
-    <h2 [ngClass]="messageClasses">{{ greetUser() }}</h2>
+    <!-- <h2 [ngClass]="messageClasses">{{ greetUser() }}</h2> -->
     <!-- class binding with condition -->
-    <h2 [class.text-danger]="hasError">{{ siteUrl }}</h2>
-    <input [disabled]="isDisabled" [id]="myId" type="text" value="henry" />
+    <!-- <h2 [class.text-danger]="hasError">{{ siteUrl }}</h2>
+    <input [disabled]="isDisabled" [id]="myId" type="text" value="henry" /> -->
     <!-- inline style bimding -->
-    <h2 [style.color]="'blue'">Style binding</h2>
+    <!-- <h2 [style.color]="'blue'">Style binding</h2>
 
     inline style binding with condition
     <h2 [style.color]="hasError ? 'red' : 'green'">
@@ -27,7 +27,14 @@ import { Component } from '@angular/core';
     <h3 [ngStyle]="titleStyle">style binding 3</h3>
 
     <p>{{greetings}}</p>
-    <button (click)="sayHello($event)">Greet</button> `,
+    <button (click)="sayHello($event)">Greet</button> 
+     -->
+    
+     <!-- <input type="text" #myInput>
+     <button (click)="logMessage(myInput)" >Log</button> -->
+
+     <input [(ngModel)]="callName" type="text" >{{callName}}
+    `,
 
   styles: [
     `
@@ -46,6 +53,7 @@ import { Component } from '@angular/core';
 })
 export class TestComponent {
   public name = 'henry';
+  public callName = '';
   public siteUrl = window.location.href;
   public myId = 'testId';
   public isDisabled = false;
@@ -73,4 +81,9 @@ export class TestComponent {
     console.log(event);
     this.greetings = "hello menez"
   }
+
+  logMessage(value: any){
+    console.log(value);
+  }
+
 }
