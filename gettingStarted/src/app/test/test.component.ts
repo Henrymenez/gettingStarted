e71915d27971d91f73403a81096326c5592177cd
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2023-04-17 07:00:46 +0100

    ng directives

--- gettingStarted/src/app/test/test.component.ts
+++ gettingStarted/src/app/test/test.component.ts
@@ -3,13 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: '.app-test',
   template: `
-  <h2 *ngIf="displayName">Here here</h2>
-  
-  
+    <!-- <h2 *ngIf="displayName; else elseBlock">Here here</h2>
+    <ng-template #elseBlock>
+      <h2>Name is hidden</h2>
+    </ng-template>
+
+
+    <div *ngIf="displayName; then thenBlock; else elseBlock2"> </div>
+<ng-template #thenBlock>
+  <h2>second directive</h2>
+</ng-template>
+
+<ng-template #elseBlock2>
+  <h2>Hidden second</h2>
+</ng-template> -->
+
+<div [ngSwitch]="color">
+<div *ngSwitchCase="'red'">you picked red color</div>
+<div *ngSwitchCase="'blue'">you picked blue color</div>
+<div *ngSwitchCase="'green'">you picked green color</div>
+<div *ngSwitchDefault>Pick again</div>
+</div>
+
   `,
-  styles: []
+  styles: [],
 })
 export class TestComponent {
-
-  public displayName = false;
+  public displayName = true;
+  public color = 'her';
 }
